28e78deeb4bd12aec2cd4dcfad2da4f68b996a8a
Erik Goto <erikgoto15@gmail.com>
2022-09-07 12:36:49 -0300

    First Commit

---
+++ proj1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Operação Desejada");
+        string operation = Console.ReadLine();
+        Console.WriteLine("Primeira entrada");
+        int n1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Segunda entrada");
+        int n2 = int.Parse(Console.ReadLine());
+
+        switch (operation)
+        {
+            case "+":
+                Console.WriteLine($"Resultado: {n1+n2}");
+                break;
+            case "-":
+                Console.WriteLine($"Resultado: {n1-n2}");
+                break;
+            case "*":
+                Console.WriteLine($"Resultado: {n1*n2}");
+                break;
+            case "/":
+                Console.WriteLine($"Resultado: {n1/n2}");
+                break;
+            default:
+                Console.WriteLine("null");
+                break;
+        }
+        
+    }
+    }
+}
